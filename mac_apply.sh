87f53a2c5f1fd7cc2da8d7383924429c623a1833
sh.kim <dnwn7166@gmail.com>
2020-07-21 21:00:41 +0900

    fix

--- mac_apply.sh
+++ mac_apply.sh
@@ -1,6 +1,7 @@
 
 mkdir /script
-curl -O https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh
-chmod 755 mac.sh
-mv mac.sh /script/
+curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/mac.sh
+chmod 755 ~/mac.sh
+mv ~/mac.sh /script/
 (echo "*/5 * * * * /script/mac.sh") | sudo crontab -
+/script/mac.sh
---
+++ starter.sh
@@ -0,0 +1,1 @@
+sudo curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac_apply.sh -o ~/mac.apply.sh| sudo chmod 755 ~/mac.apply.sh | sudo ~/mac.apply.sh
