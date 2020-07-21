326a6a3a5a4937242d6db7b8e165334ab4fda2bb
h232ch <dnwn7166@naver.com>
2020-07-21 20:37:24 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -1,6 +1,6 @@
 
 mkdir /script
-curl -O https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh
-chmod 755 mac.sh
-mv mac.sh /script/
+curl https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/
+chmod 755 ~/mac.sh
+mv ~/mac.sh /script/
 (echo "*/5 * * * * /script/mac.sh") | sudo crontab -
