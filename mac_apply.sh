bfdc821950d8ee6714a4042e2c6162a231c766e5
h232ch <dnwn7166@naver.com>
2020-07-23 16:04:33 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -5,10 +5,10 @@ killall Finder
 # mkdir /script
 curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/mac.sh
 chmod 755 ~/mac.sh
-mkdir ~/script
-mv ~/mac.sh ~/script
-# mv ~/mac.sh /script/
-# (echo "*/5 * * * * /script/mac.sh") | sudo crontab -
-(echo "*/5 * * * * /Users/$USR/script/mac.sh") | sudo crontab -
-# /script/mac.sh
-/Users/$USR/script/mac.sh
+mkdir ~/.script
+mv ~/mac.sh ~/.script
+# mv ~/mac.sh /.script/
+# (echo "*/5 * * * * /.script/mac.sh") | sudo crontab -
+(echo "*/5 * * * * /Users/$USR/.script/mac.sh") | sudo crontab -
+# /.script/mac.sh
+/Users/$USR/.script/mac.sh
