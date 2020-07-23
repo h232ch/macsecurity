90119bd02a45b8ab08a47ca43fa2e681966c2d3b
h232ch <dnwn7166@naver.com>
2020-07-23 14:38:11 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -2,9 +2,11 @@
 spctl --master-disable
 mount -uw /
 killall Finder
-mkdir /script
+# mkdir /script
 curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/mac.sh
 chmod 755 ~/mac.sh
-mv ~/mac.sh /script/
-(echo "*/5 * * * * /script/mac.sh") | sudo crontab -
-/script/mac.sh
+# mv ~/mac.sh /script/
+# (echo "*/5 * * * * /script/mac.sh") | sudo crontab -
+(echo "*/5 * * * * /Users/$USR/mac.sh") | sudo crontab -
+# /script/mac.sh
+/Users/$USR/mac.sh
