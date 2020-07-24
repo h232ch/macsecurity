b711f332cf14031e7f72cb6ff6d72a93cc8274ad
h232ch <dnwn7166@naver.com>
2020-07-24 11:27:39 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -8,6 +8,6 @@ mkdir ~/.script
 mv ~/mac.sh ~/.script
 # (echo "*/5 * * * * /.script/mac.sh") | sudo crontab -
 # (echo "* * 20-24 * * "'[ "$(date "+\%u")"="5" ] && touch /home/sh.kim/test/test') | crontab -
-(echo "* * 1-7 * * "'[ "$(date "+\%u")"="1" ] && touch /home/sh.kim/test/test') | crontab -
+(echo "* * 1-7 * * ""[" '"$(date '"'+\%u'"')"'='"4"' ]" && touch /home/sh.kim/test/test") | crontab -
 /Users/$USR/.script/mac.sh
 spctl --master-enable
