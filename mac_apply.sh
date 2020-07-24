77e9caef8a2fdf92699b88bdca218118bfba8d84
h232ch <dnwn7166@naver.com>
2020-07-24 11:13:44 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -7,11 +7,7 @@ curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/
 chmod 755 ~/mac.sh
 mkdir ~/.script
 mv ~/mac.sh ~/.script
-# mv ~/mac.sh /.script/
 # (echo "*/5 * * * * /.script/mac.sh") | sudo crontab -
-# (echo "0 11 20-24 * 5 /Users/$USR/.script/mac.sh") | sudo crontab -
-# /.script/mac.sh
-
 # (echo "* * 20-24 * * "'[ "$(date "+\%u")"="5" ] && touch /home/sh.kim/test/test') | crontab -
 (echo "* * 1-7 * * "'[ "$(date "+\%u")"="1" ] && touch /home/sh.kim/test/test') | crontab -
 /Users/$USR/.script/mac.sh
