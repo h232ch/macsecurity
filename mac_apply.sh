a2e8282f60c1ddc4741ba2498c7f93fde36b1ef8
h232ch <dnwn7166@naver.com>
2021-05-16 13:32:30 +0900

    Update mac_apply.sh

--- mac_apply.sh
+++ mac_apply.sh
@@ -5,20 +5,18 @@ if [ -d $SCHOME ]; then
 	rm -rf $SCHOME
 fi
 
-# curl -L http://172.16.10.145/wazuh-agent-latest.pkg -o ~/wazuh-agent-latest.pkg
-# launchctl setenv WAZUH_MANAGER “172.16.10.180” && installer -pkg ~/wazuh-agent-latest.pkg -target /
-# rm -rf ~/wazuh-agent-latest.pkg
-
 spctl --master-disable
 mount -uw /
 killall Finder
-# curl -L http://172.16.10.145/mac.sh -o ~/mac.sh
+
 curl -L https://raw.githubusercontent.com/h232ch/macsecurity/master/mac.sh -o ~/mac.sh
 chmod 755 ~/mac.sh
 mkdir ~/.script
 mv ~/mac.sh ~/.script
+
 # (echo "*/5 * * * * /.script/mac.sh") | sudo crontab -
 # (echo "* * 20-24 * * ""[" '"$(date '"'+\%u'"')" '=' "5"' ]" && touch /home/sh.kim/test/test")  | crontab -
+
 crontab -r
 (echo "0 15 1-7 * * ""[" '"$(date '"'+\%u'"')" '=' "1"' ]" && /Users/$USR/.script/mac.sh") | crontab -
 /Users/$USR/.script/mac.sh
